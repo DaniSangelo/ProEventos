37a37abdb5d77794909d3b45c6481c32cc4b6f43
Daniel Sângelo Passos <dpassos@nelogica.com.br>
2021-05-31 19:29:34 -0300

    Criando a primeira migration e o context

--- Back/src/ProEventos.API/Controllers/EventoController.cs
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using ProEventos.API.Data;
 using ProEventos.API.Model;
 
 namespace ProEventos.API.Controllers
@@ -12,41 +14,22 @@ namespace ProEventos.API.Controllers
     [Route("api/[controller]")]
     public class EventoController : ControllerBase
     {
-        private IEnumerable<Evento> _evento = new Evento []{
-            new Evento() 
-            {
-                EventoId = 1,
-                Tema = "Angular 11 e .NET 5",
-                Local = "Belo Horizonte",
-                Lote = "1º Lote",
-                QtdPessoas = 250,
-                DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy")
-            },
-            new Evento()
-            {
-                EventoId = 2,
-                Tema = "Angular e suas novidades",
-                Local = "São Paulo",
-                Lote = "2º Lote",
-                QtdPessoas = 300,
-                DataEvento = DateTime.Now.AddDays(4).ToString("dd/MM/yyyy")
-            }
-        };
-        public EventoController()
+        private readonly DataContext _context;
+        public EventoController(DataContext context)
         {
-            
+            _context = context;
         }
 
         [HttpGet]
         public IEnumerable<Evento> Get()
         {
-            return _evento;
+            return _context.Eventos;
         }
 
         [HttpGet("{id}")]
-        public IEnumerable<Evento> Get(int eventoId)
+        public Evento Get(int eventoId)
         {
-            return _evento.Where(e => e.EventoId == eventoId);
+            return _context.Eventos.FirstOrDefault(evento => evento.EventoId == eventoId);
         }
     }
 }
