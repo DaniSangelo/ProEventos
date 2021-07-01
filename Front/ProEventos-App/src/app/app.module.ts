afb0c773b198e2c2809706585d8b9b68e0e49e60
Daniel Sângelo Passos <dpassos@nelogica.com.br>
2021-07-01 20:30:35 -0300

    criando as rotas dos componentes user, login e registration

--- Front/ProEventos-App/src/app/app.module.ts
+++ Front/ProEventos-App/src/app/app.module.ts
@@ -20,7 +20,7 @@ import {ModalModule} from 'ngx-bootstrap/modal'
 import {ToastrModule} from 'ngx-toastr'
 import {NgxSpinnerModule} from 'ngx-spinner'
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { PerfilComponent } from './components/perfil/perfil.component';
+import { PerfilComponent } from './components/user/perfil/perfil.component';
 import { ContatosComponent } from './components/contatos/contatos.component';
 import { EventoDetalheComponent } from './components/eventos/evento-detalhe/evento-detalhe.component';
 import { EventoListaComponent } from './components/eventos/evento-lista/evento-lista.component';
