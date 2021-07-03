b7040036e9f1b764def68684ba3b75bc560b983f
Daniel Sângelo Passos <dpassos@nelogica.com.br>
2021-07-03 14:29:28 -0300

    Fazendo a reatividade do form de perfil

--- Front/ProEventos-App/src/app/components/user/perfil/perfil.component.ts
+++ Front/ProEventos-App/src/app/components/user/perfil/perfil.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControlOptions, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ValidatorField } from '@app/helpers/ValidatorField';
 
 @Component({
   selector: 'app-perfil',
@@ -8,10 +10,55 @@ import { Component, OnInit } from '@angular/core';
 export class PerfilComponent implements OnInit {
 
   currentPage: string = 'Perfil';
+  form!: FormGroup;
 
-  constructor() { }
-
-  ngOnInit() {
+  get f(): any{
+    return this.form.controls;
   }
 
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.validation();
+  }
+
+  private validation(): void{
+    const formOptions: AbstractControlOptions = {
+      validators: ValidatorField.MustMatch('password','passwordConfirmation')
+    };
+
+    this.form = this.formBuilder.group({
+      firstName: ['', Validators.required] ,
+      lastName: ['', Validators.required] ,
+      email: ['',
+        [
+          Validators.required, Validators.email
+        ]
+      ] ,
+      telefone: ['', Validators.required] ,
+      descricao: ['', Validators.required] ,
+      password: ['',
+        [
+          Validators.required,
+          Validators.minLength(6)
+        ]
+     ] ,
+      passwordConfirmation: ['',
+        [
+          Validators.required,
+          Validators.minLength(6)
+        ]
+      ]
+    }, formOptions)
+  }
+
+  public resetForm(event: any): void{
+    event.preventDefault();
+    this.form.reset();
+  }
+
+  onSubmit(): void{
+    if (this.form.invalid)
+      return;
+  }
 }
